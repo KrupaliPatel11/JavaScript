99837aeb552f28d9adb1e8ce8f0478886d9d3431
Krupali Patel <krtptl1191@gmail.com>
2023-11-27 19:13:10 +0530

    Complete given task

--- flat.js
+++ flat.js
@@ -1,25 +1,9 @@
 
-// const arr = [1, 2, [3, 4], { name: "Krupali" }];
-// console.log(typeof arr[2]);
-// function flatArr(arr) {
-//   let flat = [];
-//   arr.forEach((ele) => {
-//     if (ele.isArray(arr)) {
-//         flat(ele)
-//     } else {
-//       flat.push(ele);
-//     }
-//   });
-//   return flat;
-// }
-// flatArr(arr);
-
 const arr = [1, 2, [3, 5], { name: "Krupali" }];
 function flatArr(arr) {
-  let initial = [];
   const result = arr.reduce((acc, val) => {
     acc.concat(val);
-  }, initial);
+  }, []);
   console.log(result);
   return result;
 }
